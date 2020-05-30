9eadc527ca990641cd02421f55e5b5b36196fbe0
Dacian Bujor <bdacian18@gmail.com>
2020-05-30 14:38:35 +0300

    Update...

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 sudo apt update
 sudo apt install -y git ansible
 
-sudo ansible-pull -U https://github.com/slave2anubis/educatm.git -C master
+sudo ansible-pull -U https://github.com/EducaTM/educatmos.git -C master
