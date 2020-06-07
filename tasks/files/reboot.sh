6d14b552906e5bee3b5f8b8fa5988373994e0c30
Dacian Bujor <bdacian18@gmail.com>
2020-06-07 17:53:11 +0300

    Added the stats pyhton script to the reboot cron

--- tasks/files/reboot.sh
+++ tasks/files/reboot.sh
@@ -5,6 +5,7 @@ while :; do
 
         if [ $? -eq 0 ]; then
                 /usr/bin/ansible-pull -o -U https://github.com/EducaTM/educatmos.git -C master > /home/ansible/last_log
+                /usr/bin/python3 /home/ansible/stats/stats.py
                 break
         fi
         sleep 10
