9805829363b91c9c79ff118310eb4df0b39d5c96
Xyn Dotnest <xyn.dotnest@gmail.com>
2020-12-22 12:56:40 +0200

    testing

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ sudo apt update
 sudo apt install -y git ansible
 
 sudo ansible-pull -U https://github.com/xyn/educatmos.git -C development
+
+sudo rm /etc/xdg/autostart/piwiz.desktop
+
+sudo reboot now
