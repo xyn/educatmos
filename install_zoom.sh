def4b59576c52d392065bac95eb205a900784eb3
Xyn <me@xyn.ro>
2021-02-07 12:08:47 +0200

    testing

--- install_zoom.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp files/zoom.zip /home/pi/
-cd /home/pi/
-unzip /home/pi/zoom.zip
-cp /home/pi/zoom/zoom.desktop /home/pi/Desktop/
-chmod +x /home/pi/zoom/*
-chmod +x /home/pi/Desktop/zoom.desktop
