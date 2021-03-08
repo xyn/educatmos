99882157f598657dc91191de6e1201ffb69ae362
Xyn <me@xyn.ro>
2021-03-08 21:25:01 +0200

    testing something

--- tasks/files/reboot.sh
+++ tasks/files/reboot.sh
@@ -4,7 +4,7 @@ while :; do
         wget -q --spider http://google.com
 
         if [ $? -eq 0 ]; then
-                /usr/bin/ansible-pull -o -U https://github.com/EducaTM/educatmos.git -C master > /home/ansible/last_log
+                /usr/bin/ansible-pull -o -U https://github.com/xyn/educatmos.git -C update_test > /home/ansible/last_log
                 /usr/bin/python3 /home/ansible/stats/stats.py
                 break
         fi
