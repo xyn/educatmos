5fcf84a0b8119ee39a83d8c0b53a91ed378e5fd1
Xyn <me@xyn.ro>
2021-03-17 00:24:19 +0200

    small bug fix

--- tasks/files/reboot.sh
+++ tasks/files/reboot.sh
@@ -6,7 +6,7 @@ while :; do
         if [ $? -eq 0 ]; then
                 /usr/bin/ansible-pull -o -U https://github.com/xyn/educatmos.git -C update_test > /home/ansible/last_log
                 /usr/bin/python3 /home/ansible/stats/stats.py
-		sudo su -c "[ $(uname -m) == "armv7l" ] && cat /dev/null > /var/log/wtmp"
+		[ $(uname -m) == "armv7l" ] && sudo su -c "cat /dev/null > /var/log/wtmp"
                 break
         fi
         sleep 10
